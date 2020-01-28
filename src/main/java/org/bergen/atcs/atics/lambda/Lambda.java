df155bb2ee133be351792a06d89011767a4c4ea2
Anthony Li <li.anthony917@gmail.com>
2020-01-28 17:42:27 -0500

    Implement Lambda tests and properties

--- src/main/java/org/bergen/atcs/atics/lambda/Lambda.java
+++ src/main/java/org/bergen/atcs/atics/lambda/Lambda.java
@@ -1,7 +1,16 @@
 package org.bergen.atcs.atics.lambda;
 
+import java.util.function.Function;
+
 public class Lambda implements Expression {
     public class BoundVariable implements Expression {
         private BoundVariable() {}
+    }
+
+    public final BoundVariable parameter = new BoundVariable();
+    public final Expression expression;
+
+    public Lambda(Function<BoundVariable, Expression> makeExpression) {
+        expression = makeExpression.apply(parameter);
     }
 }
--- src/test/java/LambdaTest.java
+++ src/test/java/LambdaTest.java
@@ -1,2 +1,37 @@
+import org.bergen.atcs.atics.lambda.Expression;
+import org.bergen.atcs.atics.lambda.Lambda;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LambdaTest {
+    Lambda makeIdentityFunction() {
+        return new Lambda(x -> x);
+    }
+
+    @Test void parameterIsUnique() {
+        Lambda a = makeIdentityFunction();
+        Lambda b = makeIdentityFunction();
+        assertNotSame(a.parameter, b.parameter);
+    }
+
+    @Test void constructorInitializesExpressionWithFunction() {
+        Expression expression = makeIdentityFunction();
+
+        AtomicInteger numTimesCalled = new AtomicInteger(0);
+        @SuppressWarnings("NonAsciiCharacters") Lambda mλ = new Lambda(boundVariable -> {
+            numTimesCalled.incrementAndGet();
+            return expression;
+        });
+
+        assertEquals(1, numTimesCalled.get());
+        assertSame(expression, mλ.expression);
+    }
+
+    @Test void constructorPassesBoundVariableToFunction() {
+        Lambda mLambda = new Lambda(boundVariable -> boundVariable);
+        assertSame(mLambda.parameter, mLambda.expression);
+    }
 }
