ac3ffb4b44ee8c35950bd6fece76edc371bfe67d
Anthony Li <li.anthony917@gmail.com>
2020-02-04 09:07:33 -0500

    Remove comment

--- src/main/java/org/bergen/atcs/atics/lambda/Lambda.java
+++ src/main/java/org/bergen/atcs/atics/lambda/Lambda.java
@@ -3,11 +3,6 @@ package org.bergen.atcs.atics.lambda;
 import java.util.function.Function;
 
 public class Lambda implements Expression {
-    // BoundVariable is no longer used when evaluating expressions.
-    // It will be moved to the lambda serializer, which will use
-    // the class as a placeholder for the true value of the bound
-    // variable.
-
     public final Function<Expression, Expression> makeExpression;
 
     public Lambda(Function<Expression, Expression> makeExpression) {
